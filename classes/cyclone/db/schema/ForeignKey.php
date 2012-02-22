6476451d792f7e428f58c80dc20f5c15f39971d4
Bence Eros <crystal@cyclonephp.org>
2012-02-22 11:44:01 +0100

    improved foreign key handling

--- classes/cyclone/db/schema/ForeignKey.php
+++ classes/cyclone/db/schema/ForeignKey.php
@@ -2,6 +2,8 @@
 
 namespace cyclone\db\schema;
 
+use cyclone\db;
+
 /**
  * @author Bence Eros <crystal@cyclonephp.org>
  * @package DB
@@ -27,5 +29,42 @@ class ForeignKey {
      * @var array<Column>
      */
     public $foreign_columns = array();
+
+    public function equals(ForeignKey $other) {
+        if ($this->local_table != $other->local_table)
+            return FALSE;
+
+        if ($this->foreign_table != $other->foreign_table)
+            return FALSE;
+
+        if (count($this->local_columns) != count($other->local_columns))
+            return FALSE;
+
+        if (count($this->foreign_columns) != count($other->foreign_columns))
+            return FALSE;
+
+        if (count($this->local_columns) != count($this->foreign_columns))
+            throw new db\Exception("invalid foreign key state: " . count($this->local_columns)
+                    . " local columns can not reference " . count($this->foreign_columns)
+                    . " foreign columns");
+
+        foreach ($this->local_columns as $idx => $col) {
+            $other_idx = array_search($col, $other->local_columns);
+            if (FALSE === $other_idx)
+                return FALSE;
+
+            $foreign_col = $this->foreign_columns[$idx];
+
+
+
+            if ($col != $other->local_columns[$other_idx])
+                return FALSE;
+
+            if ($foreign_col != $other->foreign_columns[$other_idx])
+                return FALSE;
+        }
+
+        return TRUE;
+    }
 
 }
--- classes/cyclone/db/schema/Table.php
+++ classes/cyclone/db/schema/Table.php
@@ -7,6 +7,7 @@ use cyclone\db;
 /**
  * @author Bence Eros <crystal@cyclonephp.com>
  * @package DB
+ * @property-read array<ForeignKey> $foreign_keys
  */
 class Table {
 
@@ -30,16 +31,6 @@ class Table {
         self::$_instances[$name] = $this;
         $this->name = $name;
     }
-
-    /**
-     *
-     * @param string $name
-     * @return Column
-     */
-    public function create_column($name) {
-        return $this->columns []= new Column($this, $name);
-    }
-
 
     public static function for_record_schema(db\record\Schema $schema) {
         $rval = new Table;
@@ -75,5 +66,64 @@ class Table {
      */
     public $columns = array();
 
-    public $foreign_keys = array();
+    /**
+     * A sequence of foreign keys associated with the table.
+     *
+     * @var array<ForeignKey>
+     * @usedby add_foreign_key()
+     */
+    public $_foreign_keys = array();
+
+    private static $_enabled_attributes = array(
+        'foreign_keys'
+    );
+
+    public function __get($name) {
+        if (in_array($name, self::$_enabled_attributes))
+            return $this->{'_' . $name};
+            
+        throw new db\Exception("property $name doesn't exist or is not readable");
+    }
+
+    /**
+     *
+     * @param string $name
+     * @return Column
+     */
+    public function create_column($name) {
+        return $this->columns []= new Column($this, $name);
+    }
+
+    /**
+     * Adds a new foreign key object to \c $foreign_keys if the new foreign key
+     * is not present yet.
+     *
+     * If the <code>$local_table</code> of the foreign key is <code>NULL</code>
+     * then it will default to <code>$this</code>. Otherwise if the local table of
+     * the foreign key is not <code>$this</code> then a \c \cyclone\db\Exception
+     * will be thrown.
+     *
+     * @param ForeignKey $new_fk
+     * @uses Foreignkey::equals()
+     * @see ForeignKey::$local_table
+     */
+    public function add_foreign_key(ForeignKey $new_fk) {
+        $exists = FALSE;
+        foreach ($this->foreign_keys as $old_fk) {
+            if ($old_fk->equals($new_fk)) {
+                $exists = TRUE;
+                break;
+            }
+        }
+        if ($exists)
+            return;
+
+        if (NULL === $new_fk->local_table) {
+            $new_fk->local_table = $this;
+        }
+        if ($new_fk->local_table !== $this)
+            throw new db\Exception("could not attach a foreign key to table {$this->name} with local table {$new_fk->local_table->name}");
+
+        $this->foreign_keys []= $new_fk;
+    }
 }
