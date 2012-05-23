31bc8c79bb957ae0e03ffff584b35b318922718d
Bence Eros <crystal@cyclonephp.org>
2012-05-23 12:28:48 +0200

    making postgre prepared executor prepare() method failsafe and implementing exec_select()

--- classes/cyclone/db/prepared/executor/Postgres.php
+++ classes/cyclone/db/prepared/executor/Postgres.php
@@ -2,6 +2,7 @@
 namespace cyclone\db\prepared\executor;
 
 use cyclone\db;
+use cyclone as cy;
 /**
  * @author Bence Erős <crystal@cyclonephp.org>
  * @package DB
@@ -9,12 +10,20 @@ use cyclone\db;
 class Postgres extends AbstractPreparedExecutor {
 
     public function prepare($sql) {
-        return pg_prepare($this->_db_conn, $sql, $sql);
+        $rval = @pg_prepare($this->_db_conn, $sql, $sql);
+        if (FALSE === $rval) {
+            throw new db\Exception('failed to prepare statement: ' . $sql
+                 . ' (' . pg_last_error($this->_db_conn) . ')');
+        }
+        return $rval;
     }
 
     public function exec_select($prepared_stmt, array $params
-        , db\query\Select $orig_query) {
-        throw new \Exception('not implemented');
+            , db\query\Select $orig_query) {
+        $sql = cy\DB::compiler($this->_config['config_name'])->compile_select($orig_query);
+        $result = pg_execute($this->_db_conn, $sql, $params);
+
+        return new db\query\result\Postgres($result);
     }
 
     public function exec_delete($prepared_stmt, array $params) {
--- tests/DB/Postgres/PreparedTest.php
+++ tests/DB/Postgres/PreparedTest.php
@@ -12,4 +12,16 @@ class DB_Postgres_PreparedTest extends Kohana_Unittest_TestCase {
             ->prepare("select * from users")), "\\cyclone\\db\\prepared\\executor\\Postgres::prepare() returns a resource");
     }
 
+    /**
+     * @expectedException cyclone\db\Exception
+     */
+    public function test_prepare_failure() {
+        $stmt = cy\DB::executor_prepared('cytst-postgres')->prepare('select * from dummy');
+    }
+
+    public function test_exec_select() {
+        $result = cy\DB::select('id', 'name')->from('users')->prepare('cytst-postgres')->exec();
+        $this->assertEquals(2, count($result));
+    }
+
 }
