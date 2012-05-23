d7d97bf7f1db7f5981a74b5b6c9b49e9ca09231e
Bence Eros <crystal@cyclonephp.org>
2012-05-23 12:55:10 +0200

    extracting StmtResult creation from db\executor\Mysqli::exec_select() - to be reused by prepared executor

--- classes/cyclone/db/executor/Mysqli.php
+++ classes/cyclone/db/executor/Mysqli.php
@@ -19,24 +19,22 @@ class Mysqli extends AbstractExecutor {
         return new db\query\result\Mysqli($result);
     }
 
-    public function  exec_insert($sql, query\Insert $orig_query = NULL) {
-        if ( ! $this->_db_conn->query($sql))
-            throw new db\Exception($this->_db_conn->error, $this->_db_conn->errno);
-
-        $returning_clause = $orig_query->returning;
-        $affected_rows = $this->_db_conn->affected_rows;
+    public static function stmt_result_for_insert(query\Insert $insert
+            , $config_name
+            , $affected_rows
+            , $insert_id) {
+        $returning_clause = $insert->returning;
         $rows = array();
         if (count($returning_clause) == 1) {
-            $rows[0] = array($returning_clause[0] => $this->_db_conn->insert_id);
+            $rows[0] = array($returning_clause[0] => $insert_id);
         } elseif (count($returning_clause) > 1) {
-            $insert_id = $this->_db_conn->insert_id;
             $where_cond_column = array_shift($returning_clause);
-            $query = cy\DB::select()->from($orig_query->table)
+            $query = cy\DB::select()->from($insert->table)
                 ->where($where_cond_column, '=', cy\DB::esc($insert_id));
             $query->columns = $returning_clause;
-            $compiler = cy\DB::compiler($this->_config['config_name']);
+            $compiler = cy\DB::compiler($config_name);
             $sql = $compiler->compile_select($query);
-            $result = $this->exec_select($sql);
+            $result = cy\DB::executor($config_name)->exec_select($sql);
             if (count($result) == 0)
                 throw new db\Exception('failed to retrieve inserted columns: "' . implode(', ', $orig_query->returning). '"');
             $result = $result->as_array();
@@ -45,6 +43,16 @@ class Mysqli extends AbstractExecutor {
             $rows[0] = $result;
         }
         return new db\StmtResult($rows, $affected_rows);
+    }
+
+    public function  exec_insert($sql, query\Insert $orig_query = NULL) {
+        if ( ! $this->_db_conn->query($sql))
+            throw new db\Exception($this->_db_conn->error, $this->_db_conn->errno);
+
+        return self::stmt_result_for_insert($orig_query
+            , $this->_config['config_name']
+            , $this->_db_conn->affected_rows
+            , $this->_db_conn->insert_id);
     }
 
     public function  exec_update($sql, query\Update $orig_query = NULL) {
