3aeb8136104cb77f5fc76649025dad5f7dcfe1c4
Bence Eros <crystal@cyclonephp.org>
2012-05-24 19:56:19 +0200

    initial implementation of mysql constraint violation error message parsing

--- classes/cyclone/db/executor/MysqlExceptionBuilder.php
+++ classes/cyclone/db/executor/MysqlExceptionBuilder.php
@@ -16,6 +16,12 @@ class MysqlExceptionBuilder {
 
     const MISSING_FUNCTION = 1305;
 
+    const NOTNULL_CONSTRAINT = 1048;
+
+    const UNIQUE_CONSTRAINT  = 1062;
+
+    const FOREIGNKEY_CONSTRAINT = 1452;
+
     public static function for_error($err_msg, $errno, $sql = '') {
         switch ($errno) {
             case self::MISSING_RELATION:
@@ -31,5 +37,30 @@ class MysqlExceptionBuilder {
                 $fn_name = substr($substr, 0, strpos($substr, ' '));
                 return new db\SchemaFunctionException($err_msg, $errno, $fn_name);
         }
+
+        $builder = new db\ConstraintExceptionBuilder($err_msg . ' (' . $sql . ')');
+        $builder->message = $err_msg;
+        $builder->errcode = $errno;
+        switch ($errno) {
+            case self::NOTNULL_CONSTRAINT:
+                $substr = substr($err_msg, strpos($err_msg, "'") + 1);
+                $col_name = substr($substr, 0, strpos($substr, "'"));
+                $builder->constraint_type = db\ConstraintException::NOTNULL_CONSTRAINT;
+                $builder->column = $col_name;
+                break;
+            case self::UNIQUE_CONSTRAINT:
+                $builder->constraint_type = db\ConstraintException::UNIQUE_CONSTRAINT;
+                $parts = explode("'", $err_msg);
+                $builder->column = $parts[3];;
+                break;
+            case self::FOREIGNKEY_CONSTRAINT:
+                $parts = explode('`', $err_msg);
+                $builder->constraint_name = $parts[5];
+                $builder->column = $parts[7];
+                $builder->constraint_type = db\ConstraintException::FOREIGNKEY_CONSTRAINT;
+                break;
+        }
+        return $builder->build_exception();
     }
+
 }
---
+++ tests/DB/Mysqli/ConstraintTest.php
@@ -0,0 +1,91 @@
+<?php
+
+use cyclone as cy;
+use cyclone\db;
+
+require_once __DIR__ . '/DBTest.php';
+
+/**
+ * @author Bence Erős <crystal@cyclonephp.org>
+ */
+class DB_Mysqli_ConstraintTest extends DB_Mysqli_DbTest {
+
+    /**
+     * @dataProvider provider_constraint_exceptions
+     */
+    public function test_constraint_exceptions($err_str, $errno, $expected_params) {
+        $ex = db\executor\MysqlExceptionBuilder::for_error($err_str, $errno);
+        $this->assertEquals('cyclone\\db\\ConstraintException', get_class($ex)
+            , 'ConstraintException instance is created');
+        foreach ($expected_params as $k => $v) {
+            if ($k != 'class') {
+                $this->assertEquals($v, $ex->$k);
+            }
+        }
+    }
+
+    public function provider_constraint_exceptions() {
+        return array(
+            array('Column \'name\' cannot be null'
+            , 1048
+            , array(
+                'constraint_type' => db\ConstraintException::NOTNULL_CONSTRAINT,
+                'column' => 'name'
+            )),
+            array('Duplicate entry \'u@e\' for key \'email\''
+            , 1062
+            , array(
+                'constraint_type' => db\ConstraintException::UNIQUE_CONSTRAINT,
+                'constraint_name' => NULL,
+                'column' => 'email'
+            )),
+            array(
+                'Cannot add or update a child row: a foreign key constraint fails '
+                    . '(`simpledb`.`t_posts`, CONSTRAINT `t_posts_ibfk_1` FOREIGN KEY (`user_fk`)'
+                    . ' REFERENCES `cy_user` (`id`))'
+                , 1452
+                , array(
+                'constraint_type' => db\ConstraintException::FOREIGNKEY_CONSTRAINT,
+                'constraint_name' => 't_posts_ibfk_1',
+                'column' => 'user_fk'
+            ))/*,
+            array(
+                'ERROR:  duplicate key value violates unique constraint "uniq_constr"
+DETAIL:  Key (uniq_named)=(5) already exists.', array(
+                'constraint_type' => db\ConstraintException::UNIQUE_CONSTRAINT,
+                'constraint_name' => 'uniq_constr',
+                'column' => 'uniq_named'
+            )),
+            array(
+                'ERROR:  new row for relation "chk_constraints" violates check constraint "chk_constraints_chk_unnamed_check"',
+                array(
+                    'constraint_type' => db\ConstraintException::APP_CONSTRAINT,
+                    'constraint_name' => 'chk_constraints_chk_unnamed_check',
+                ))*/
+
+        );
+    }
+
+    public function test_not_null_constraint_exception() {
+        $thrown = FALSE;
+        $query = cy\DB::update('users')->values(array('name' => null));
+        try {
+            $query->exec('cytst-mysqli');
+        } catch (db\ConstraintException $ex) {
+            $this->assertEquals(db\ConstraintException::NOTNULL_CONSTRAINT, $ex->constraint_type);
+            $this->assertEquals('name', $ex->column);
+            $thrown = TRUE;
+        }
+        $this->assertTrue($thrown, 'ConstraintException thrown');
+
+        try {
+            $query->prepare('cytst-mysqli')->exec();
+        } catch (db\ConstraintException $ex) {
+            $this->assertEquals(db\ConstraintException::NOTNULL_CONSTRAINT, $ex->constraint_type);
+            $this->assertEquals('name', $ex->column);
+            $thrown = TRUE;
+        }
+        $this->assertTrue($thrown, 'ConstraintException for prepared stmt thrown');
+    }
+
+}
